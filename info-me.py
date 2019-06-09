3b09950beb43a93fc61c710d55a969f299f43145
i3z101 <abofahad,en@gmail.com>
2019-06-09 08:39:57 +0800

    Edit BY PYTHON

--- info-me.py
+++ info-me.py
@@ -2,4 +2,60 @@ Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD6
 Type "copyright", "credits" or "license()" for more information.
 >>> print("hello")
 hello
+>>> def ali():
+	name= input("name? ")
+
+	
+>>> ali()
+name? abdulaziz
+>>> def ali():
+	name= input("name? ")
+	print(name.upper())
+
+	
+>>> def ali():
+	name= input("name? ")
+	print(name.upper(1))
+
+	
+>>> ali()
+name? abdulaziz
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    ali()
+  File "<pyshell#9>", line 3, in ali
+    print(name.upper(1))
+TypeError: upper() takes no arguments (1 given)
+>>> def ali():
+	name= input("name? ")
+	print(name.upper())
+
+	
+>>> ali()
+name? ali
+ALI
+>>> def ali():
+	name= input("name? ")
+	print(name[0].upper())
+
+	
+>>> ali()
+name? aaaaaaa
+A
+>>> def ali():
+	name= input("name? ")
+	print(name[0].upper()+name[1])
+
+	
+>>> ali()
+name? qaqsw
+Qa
+>>> def ali():
+	name= input("name? ")
+	print(name[0].upper()+name[1:])
+
+	
+>>> ali()
+name? aknewiuricbjwer
+Aknewiuricbjwer
 >>> 
